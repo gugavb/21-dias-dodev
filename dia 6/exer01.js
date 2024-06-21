0a396029a49a6c5f3944bf4f639bda9ed812ddc8
Gustavo Vieira <gustavoabc2005@hotmail.com>
2024-06-21 17:45:31 -0300

    Dia 6 e fazendo exercicios

---
+++ dia 6/exer01.js
@@ -0,0 +1,5 @@
+let valor = parseInt(prompt('Insira um valor inteiro e positivo'))
+
+for(let i = 0; i <= valor; i++){
+    console.log(i);
+}
---
+++ dia 6/index.js
@@ -0,0 +1,6 @@
+
+let tabuada = Number(prompt('Escolha a tabuada que deseja'))
+
+for(let i = 0; i<10 ; i++){
+    console.log(tabuada + ' x ' + i + ' = ' + tabuada * i);
+}
